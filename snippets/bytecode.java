7a9abe5b615fccb368ecc28fa449923a686dcbdb
P4ntomime <laurinheitzer@gmail.com>
2024-01-07 13:11:07 +0100

    fixed overlapping text in jvm.tex, added line numbers and indentation to java code in jvm.tex

--- snippets/bytecode.java
+++ snippets/bytecode.java
@@ -2,7 +2,7 @@
 !\aftergroup\greenlst!int p = 1;!\aftergroup\endlstcol!
 !\aftergroup\redlst!int i = 1;!\aftergroup\endlstcol!
 !\aftergroup\ywlst!while (i <= n) {!\aftergroup\endlstcol!
-p = p * i!\aftergroup\ywlst!;!\aftergroup\endlstcol!
-i = i + 1!\aftergroup\ywlst!;!\aftergroup\endlstcol!
+    p = p * i!\aftergroup\ywlst!;!\aftergroup\endlstcol!
+    i = i + 1!\aftergroup\ywlst!;!\aftergroup\endlstcol!
 !\aftergroup\ywlst!}!\aftergroup\endlstcol! 
 System.out.println(p);
